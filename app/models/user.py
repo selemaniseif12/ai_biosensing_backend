def4f25daf2d481c606d8bcb035d948fd9b63b15
Selemani Seif Mziray <selemaniseif12@yahoo.com>
2026-09-10 11:54:45 -0600

    Revert CartItem and User models to match Neon schema

--- app/models/user.py
+++ app/models/user.py
@@ -11,8 +11,4 @@ class User(Base):
     full_name = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
 
-    # REMOVE this line:
-    # subscriptions = relationship("Subscription", back_populates="user")
-
-    # REMOVE this line too (already done earlier):
-    # cart_items = relationship("CartItem", back_populates="user")
+    # All relationships removed
